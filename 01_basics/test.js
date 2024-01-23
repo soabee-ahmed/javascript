d488657e050b365c684e6feed30c1cad62e83240
soabee-ahmed <130282211+soabee-ahmed@users.noreply.github.com>
2024-01-23 10:03:35 +0000

    conversion of datatypes

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -13,3 +13,36 @@ console.log(typeof currentState);
 console.log(typeof undefined)  //undefined
 console.log(typeof null);       //object    
 console.log(typeof age);
+
+//lecture =>6     CONVERSION OF DATATYPES
+let score="33"
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+let score1="33abc"
+
+let valueInNumber1=Number(score1)
+console.log(typeof valueInNumber1)
+console.log(valueInNumber1)
+
+//lecture =>7    CONFUSION OF STRING TO NUMBER CONVERSION
+let str1="hello";
+let str2=" soabee"
+console.log(str1+str2);   //hello soabee
+
+console.log("1"+2);       //12
+console.log(1+"2");      //12
+console.log("1"+2+2);    //122
+console.log(1+2+"2");   //32
+
+console.log(true);        //true
+console.log(+true);       //1
+console.log(+"");         //0
+
+let num1,num2,num3
+num1=num2=num3=2+2;       //valid in js but it is not a good approach
+console.log(num1);     
+console.log(num2);
+console.log(num3);
