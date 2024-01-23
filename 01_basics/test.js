3d3e123b1b9539f5d12db7baf45ad4b21b470ac9
soabee-ahmed <130282211+soabee-ahmed@users.noreply.github.com>
2024-01-23 09:20:16 +0000

    2 commit

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,15 @@
 console.log("Soabee")
+const accountId=134243
+let accountEmail="soabee@gmail.com"
+var account_balance=9000.87
+let account_city;
+console.table([accountId,accountEmail,account_balance,account_city])
+let age=18;
+let isLoggedIn=false;
+let state=null;
+let currentState;
+console.log(typeof state);
+console.log(typeof currentState);
+console.log(typeof undefined)  //undefined
+console.log(typeof null);       //object    
+console.log(typeof age);
