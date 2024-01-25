c394f2313857d31660cf712b3b46c53ca0b291e9
soabee-ahmed <130282211+soabee-ahmed@users.noreply.github.com>
2024-01-25 16:42:19 +0530

    while and do while loops in js

---
+++ 01_basics/whiledowhileloops.js
@@ -0,0 +1,19 @@
+//WHILE LOOP
+let i=0;
+while(i<=10){
+    console.log(`value of i is ${i}`);
+    i+=2
+}
+let myarr=['superman','flash','batman']
+let arr=0;
+while(arr<myarr.length){
+    console.log(`value is ${myarr[arr]}`);
+    arr=arr+1
+}
+
+//DO WHILE LOOP  :- it must execute atleast once even when the condition is false
+let score =1
+do{
+    console.log(`score is ${score}`);
+    score++
+}while(score<=10)
