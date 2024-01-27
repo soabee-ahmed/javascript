ffbb97c9f22152a4108c84859097c50cbf74bb52
soabee-ahmed <130282211+soabee-ahmed@users.noreply.github.com>
2024-01-28 00:10:50 +0530

    Update four.js

--- iterations/four.js
+++ iterations/four.js
@@ -1,1 +1,26 @@
+//ACCESSING OBJECTS WITH THE HELP OF FOR IN LOOP
+const myobj={
+    js:"javascript",
+    cpp:"c++",
+    rb:"ruby",
+    swift:"swift by apple"
+}
+
+for(const key in myobj){
+    console.log(`${key} shortuct is for ${myobj[key]}`);     //for accessing a value in object we use objectname[key]
+}
+
+
+//USING FOR IN LOOP IN ARRAY
+//when we use for in loop with array than when we used to print key in console.log than it prints keys of the array i.e indeces of the array 
+//but  in case of for of loop it prints the values of the array when we use key in conole.log
+
+const programming=["js","rb","py","java","cpp"]
+for(const key in programming){
+    console.log(key);                                     //0 1 2 3 4 //here it prints keys of the array i.e index of the array
+}
+for(const key in programming){
+    console.log(programming[key]);                                     //here it prints the values of the array
+}
+//we don't be able to use map with for in loop
 
