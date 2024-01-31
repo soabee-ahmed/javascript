65dcd8fed19b14c87933e42f250a89d947b5e5c0
soabee-ahmed <130282211+soabee-ahmed@users.noreply.github.com>
2024-02-01 00:05:57 +0530

    Update call.js

--- oops in js/call.js
+++ oops in js/call.js
@@ -1,1 +1,13 @@
+function SetUserName(username){
+    this.username=username;
+    console.log("called");
+}
+
+function createUser(username,email,password){
+    SetUserName.call(this,username)    //if we do not apply call function here so we do not able to initialize username if we write only setUsername(username) than its nested (setUsername) variable scope is gone so for passing its this context to setUsername we use call function
+    this.email=email
+    this.password=password
+}
+const chai=new createUser("chai","chai@gmail.com","123")
+console.log(chai)
 
