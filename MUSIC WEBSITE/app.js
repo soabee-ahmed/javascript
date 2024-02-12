6076d425e51d1933890981a5ce188415f547f807
soabee-ahmed <130282211+soabee-ahmed@users.noreply.github.com>
2024-02-13 01:29:44 +0530

    Update app.js

--- MUSIC WEBSITE/app.js
+++ MUSIC WEBSITE/app.js
@@ -159,7 +159,45 @@ const songs = [
     },
 ]
 
+//search data start
+let search_results=document.getElementsByClassName('search_results')[0];
 
+songs.forEach(element=>{
+    const {id,songName,poster}=element;
+    let card=document.createElement('a');
+    card.classList.add('card');
+    card.href="#"+id;
+    card.innerHTML=`<img src="${poster}" alt="">
+    <div class="cnt">
+        ${songName}
+    </h5>
+    </div>`;
+    search_results.appendChild(card);
+})
+let input=document.getElementsByTagName('input')[0];
+input.addEventListener('keyup',()=>{
+    let input_value=input.value.toUpperCase();
+    let items=search_results.getElementsByTagName('a');
+    for(let index=0;index,items.length;index++){
+        let as=items[index].getElementsByClassName('cnt')[0];
+        let text_value=as.textContent||as.innerHTML;
+
+        if(text_value.toUpperCase().indexOf(input_value)>-1){
+            items[index].style.display="flex";
+        }
+        else{
+            items[index].style.display="none";
+        }
+        
+        if(input.value==0){
+            search_results.style.display="none";
+        }
+        else
+            search_results.style.display="";
+    }
+})
+
+//search data end
 const music = new Audio('songs/Baari.mp3');
 //music.play();
 let masterPlay = document.getElementById('masterPlay');
@@ -378,12 +416,6 @@ shuffle.addEventListener('click', () => {
 })
 
 
-Array.from(document.getElementsByClassName('songItem')).forEach((e, i) => {
-    e.getElementsByTagName('img')[0].src = songs[i].poster;
-    e.getElementsByTagName('h5')[0].innerHTML = songs[i].songName;
-})
-
-
 const next_music=()=>{
     if(index==songs.length)
     index=1;
@@ -470,20 +502,30 @@ const random_music=()=>{
 }
 
 
-music.addEventListener('ended',()=>{
+
+music.addEventListener('ended', () => {              //when music ended than if our case matches with next than after ending of music next song is played if matches with repeat than same song is repated if random than randomly any song is played
     let b=shuffle.innerHTML;
     switch(b){
          case "next":
-             next_music();
-             break;
+            next_music();
+            break;
          case "repeat":
-             repeat_music();
-             break;
+            repeat_music();
+            break;
          case "random":
-             random_music();
-             break;
+            random_music();
+            break;
     }
- })
+})
+
+
+Array.from(document.getElementsByClassName('songItem')).forEach((e, i) => {
+    e.getElementsByTagName('img')[0].src = songs[i].poster;
+    e.getElementsByTagName('h5')[0].innerHTML = songs[i].songName;
+})
+
+
+
 
 
 
